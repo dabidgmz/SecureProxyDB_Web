035a805628ff1646c0165882fd16202f89ee8ade
David Herrera <david.gmzherrera28@gmail.com>
2024-11-27 19:57:15 -0600

    Update Enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 //Localhost
-export const environment = {
-    apiUrl: 'http://127.0.0.1:3333/api/',
-};
+// export const environment = {
+//     apiUrl: 'http://127.0.0.1:3333/api/',
+// };
 
 //Development
-/*export const environment = {
-    apiUrl: 'http://127.0.0.1:3333/api/',
-};*/
+export const environment = {
+    apiUrl: 'http://143.110.235.125/api/',
+};
